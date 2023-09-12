cf73fd6e11e31ed20daedd682ae94e7177453fc6
HafizhHW <hafizzwijaya@gmail.com>
2023-09-12 23:50:13 +0700

    Add Keliling

--- js/myJs.js
+++ js/myJs.js
@@ -1,19 +1,38 @@
 function hitungLuas() {
     // Mengambil nilai panjang sisi dari input
-    var panjangSisi = parseFloat(document.getElementById("panjangSisi").value);
+    var panjangLuasSisi = parseFloat(document.getElementById("panjangLuasSisi").value);
 
     // Menghitung luas persegi
-    var luas = panjangSisi * panjangSisi;
+    var luas = panjangLuasSisi * panjangLuasSisi;
 
     // Menampilkan hasil di elemen dengan id "hasil"
-    document.getElementById("hasil").innerHTML = "L" + "&nbsp" + "=" + "&nbsp" + "&nbsp" + luas;
-    document.getElementById("Sisi").innerHTML = "L" + "&nbsp" + "=" + "&nbsp" + panjangSisi + "&nbsp" + "x" + "&nbsp" + panjangSisi;
-    // document.getElementById("Sisi").innerHTML = "&nbsp"+panjangSisi+"&nbsp"+"x"+"&nbsp"+panjangSisi;
+    document.getElementById("hasilLuas").innerHTML = "L" + "&nbsp" + "=" + "&nbsp" + "&nbsp" + luas;
+    document.getElementById("luasSisi").innerHTML = "L" + "&nbsp" + "=" + "&nbsp" + panjangLuasSisi + "&nbsp" + "x" + "&nbsp" + panjangLuasSisi;
+
 }
 
-function resetNull() {
+function resetLuas() {
     // Mereset nilai input dan hasil
-    document.getElementById("panjangSisi").value = "";
-    document.getElementById("hasil").innerHTML = "";
-    document.getElementById("Sisi").innerHTML = "";
+    document.getElementById("panjangLuasSisi").value = "";
+    document.getElementById("hasilLuas").innerHTML = "";
+    document.getElementById("luasSisi").innerHTML = "";
+}
+
+function hitungKeliling() {
+    // Mengambil nilai panjang sisi dari input
+    var panjangKelilingSisi = parseFloat(document.getElementById("panjangKelilingSisi").value);
+
+    // Menghitung keliling persegi
+    var keliling = 4 * panjangKelilingSisi;
+
+    // Menampilkan hasil di elemen dengan id "hasilKeliling"
+    document.getElementById("hasilKeliling").innerHTML = "Keliling Persegi: " + keliling;
+    document.getElementById("kelilingSisi").innerHTML = "K" + " " + "=" + " " + "4" + " " + "x" + " " + panjangKelilingSisi;
+}
+
+function resetKeliling() {
+    // Mereset nilai input dan hasil
+    document.getElementById("panjangKelilingSisi").value = "";
+    document.getElementById("hasilKeliling").innerHTML = "";
+    document.getElementById("kelilingSisi").innerHTML = "";
 }
