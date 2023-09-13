eecce2a3183096383af22e0e5d8aac43fea26fde
HafizhHW <hafizzwijaya@gmail.com>
2023-09-13 23:32:40 +0700

    final code

--- js/myJs.js
+++ js/myJs.js
@@ -1,38 +1,74 @@
+// KELILING PERSEGI
+function hitungKeliling() {
+    // Mengambil nilai panjang sisi dari input
+    var inputKeliling = document.getElementById("panjangKelilingSisi");
+    var panjangSisiKeliling = inputKeliling.value.trim();
+
+    // Membuat pola regex untuk angka
+    var regex = /^[0-9]*$/;
+
+    // Mengecek apakah input sesuai dengan pola regex
+    if (!regex.test(panjangSisiKeliling)) {
+        document.getElementById("errorKeliling").innerHTML = "Masukkan hanya angka !";
+        inputKeliling.focus();
+        return;
+    }
+
+    // Menghapus pesan validasi jika input valid
+    document.getElementById("errorKeliling").innerHTML = "";
+
+    // Menghitung keliling persegi
+    var keliling = 4 * parseFloat(panjangSisiKeliling);
+
+    // Menampilkan hasil di elemen dengan id "hasilKeliling"
+    document.getElementById("hasilKeliling").innerHTML = "K = " + keliling;
+    document.getElementById("kelilingSisi").innerHTML = "K" + " " + "=" + " " + "4" + " " + "x" + " " + panjangSisiKeliling;
+}
+
+function resetKeliling() {
+    // Mereset nilai input, hasil, dan pesan validasi
+    document.getElementById("panjangKelilingSisi").value = "";
+    document.getElementById("hasilKeliling").innerHTML = "";
+    document.getElementById("kelilingSisi").innerHTML = "";
+    document.getElementById("errorKeliling").innerHTML = "";
+}
+
+
+// LUAS PERSEGI
+
 function hitungLuas() {
     // Mengambil nilai panjang sisi dari input
-    var panjangLuasSisi = parseFloat(document.getElementById("panjangLuasSisi").value);
+    var inputLuas = document.getElementById("panjangLuasSisi");
+    var panjangSisi = inputLuas.value.trim();
+
+    // Membuat pola regex untuk angka
+    var regex = /^[0-9]*$/;
+
+    // Mengecek apakah input sesuai dengan pola regex
+    if (!regex.test(panjangSisi)) {
+        document.getElementById("errorLuas").innerHTML = "Masukkan hanya angka !";
+        inputLuas.focus();
+        return;
+    }
+
+    // Menghapus pesan validasi jika input valid
+    document.getElementById("errorLuas").innerHTML = "";
 
     // Menghitung luas persegi
-    var luas = panjangLuasSisi * panjangLuasSisi;
+    var luas = panjangSisi * panjangSisi;
 
     // Menampilkan hasil di elemen dengan id "hasil"
-    document.getElementById("hasilLuas").innerHTML = "L" + "&nbsp" + "=" + "&nbsp" + "&nbsp" + luas;
-    document.getElementById("luasSisi").innerHTML = "L" + "&nbsp" + "=" + "&nbsp" + panjangLuasSisi + "&nbsp" + "x" + "&nbsp" + panjangLuasSisi;
+    document.getElementById("hasilLuas").innerHTML = "L = " + luas;
+    document.getElementById("luasSisi").innerHTML = "L = " + panjangSisi + " x " + panjangSisi;
 
 }
+
+
 
 function resetLuas() {
     // Mereset nilai input dan hasil
     document.getElementById("panjangLuasSisi").value = "";
     document.getElementById("hasilLuas").innerHTML = "";
     document.getElementById("luasSisi").innerHTML = "";
-}
-
-function hitungKeliling() {
-    // Mengambil nilai panjang sisi dari input
-    var panjangKelilingSisi = parseFloat(document.getElementById("panjangKelilingSisi").value);
-
-    // Menghitung keliling persegi
-    var keliling = 4 * panjangKelilingSisi;
-
-    // Menampilkan hasil di elemen dengan id "hasilKeliling"
-    document.getElementById("hasilKeliling").innerHTML = "Keliling Persegi: " + keliling;
-    document.getElementById("kelilingSisi").innerHTML = "K" + " " + "=" + " " + "4" + " " + "x" + " " + panjangKelilingSisi;
-}
-
-function resetKeliling() {
-    // Mereset nilai input dan hasil
-    document.getElementById("panjangKelilingSisi").value = "";
-    document.getElementById("hasilKeliling").innerHTML = "";
-    document.getElementById("kelilingSisi").innerHTML = "";
+    document.getElementById("errorLuas").innerHTML = "";
 }
